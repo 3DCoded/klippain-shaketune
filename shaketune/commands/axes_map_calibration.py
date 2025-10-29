65315acf062ab7a18f2a7e8cff3d9b30b3cc4e52
3DCoded <info3dcoded@gmail.com>
2025-10-29 13:45:02 -0400

    fix: Update axes_map_calibration accel_chip handling logic to be more consistent with other ShakeTune macros

--- shaketune/commands/axes_map_calibration.py
+++ shaketune/commands/axes_map_calibration.py
@@ -26,13 +26,19 @@ def axes_map_calibration(gcmd, config, st_process: ShakeTuneProcess) -> None:
     feedrate_travel = gcmd.get_float('TRAVEL_SPEED', default=120.0, minval=20.0)
     accel_chip = gcmd.get('ACCEL_CHIP', default=None)
 
+    if accel_chip == '':
+        accel_chip = None
+
     printer = config.get_printer()
     gcode = printer.lookup_object('gcode')
     toolhead = printer.lookup_object('toolhead')
     systime = printer.get_reactor().monotonic()
 
-    if not accel_chip:
+    current_accel_chip = accel_chip
+    if current_accel_chip is None:
         accel_chip = Accelerometer.find_axis_accelerometer(printer, 'xy')
+    if current_accel_chip is None:
+            raise gcmd.error('No suitable accelerometer found for measurement!')
     k_accelerometer = printer.lookup_object(accel_chip, None)
     if k_accelerometer is None:
         raise gcmd.error('Multi-accelerometer configurations are not supported for this macro!')
